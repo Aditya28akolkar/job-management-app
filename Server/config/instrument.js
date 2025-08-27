a06789471863feb8fcf29671a8b28e2549a131ba
Aditya28akolkar <adityaakolkar28@gmail.com>
2025-08-27 14:48:00 +0530

    new commit

--- Server/config/instrument.js
+++ Server/config/instrument.js
@@ -1,6 +1,6 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-import * as Sentry from "@sentry/node"
-import {nodeProfilingIntegration} from "@sentry/profiling-node";
+import * as Sentry from "../node_modules/@sentry/node/build/types"
+import {nodeProfilingIntegration} from "../node_modules/@sentry/profiling-node/build/types";
 Sentry.init({
   dsn: "https://1222f684dee4c6e5c3372da10be9c836@o4509887078793216.ingest.us.sentry.io/4509891520233472",
 
--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -1,56 +1,54 @@
-import User from "../models/User.js";
-import {Webhook} from "svix";
+import User from "./models/User.js";
+import { Webhook } from "svix";
 
 export const clerkWebhooks = async (req, res) => {
- try {
-  const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-  await whook.verify(JSON.stringify(req.body),{
-    "svix-id": req.headers["svix-id"],
-    "svix-timestamp" :req.headers["svix-timestamp"],
-    "svix-signature": req.headers["svix-signature"]
-  })
-  const {data, type}=req.body
-  switch (type){
-    case 'user.created':{
-const userData={
-  _id:data.id,
-  email: data.email_addresses[0].email_address,
-  name: data.first_name +" "+ data.last_name,
-  image:data.image_url,
-  resume:''
-}
-await User.create(userData)
-res.json({})
-break;
-    }
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
+    // Use raw buffer (req.body is a Buffer here)
+    const payload = req.body.toString("utf8");
 
-     case 'user.updated':{
-      const userData={
-  
-  email:data.email_addresses[0].email_address,
-  name:data.first_name +" "+ data.last_name,
-  image:data.image_url
-  
-}
-      await User.findByIdAndUpdate(data.id, userData)
-      res.json({})
-      break;
-    }
+    await whook.verify(payload, {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
 
-     case 'user.deleted':{
-      await User.findByIdAndDelete(data.id)
-      res.json({})
-    break;
+    const { data, type } = JSON.parse(payload);
+
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+          resume: "",
+        };
+        await User.create(userData);
+        return res.json({});
+      }
+
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        return res.json({});
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        return res.json({});
+      }
+
+      default:
+        return res.json({});
     }
-    default:
-      break;
+  } catch (error) {
+    console.error("❌ Clerk webhook error:", error.message);
+    return res.status(400).json({ success: false, message: "Webhook error" });
   }
- } catch (error) {
-  
-console.log(error.message);
-res.json({success:false,message:'Webhooks error'})
-
-
- }}
-
+};
